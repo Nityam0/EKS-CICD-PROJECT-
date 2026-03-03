d236cc4b9a9600009faf6729b1e27c9c3bf32069
Nityam Raj <147521555+Nityam0@users.noreply.github.com>
2026-03-03 15:43:15 +0530

    Refactor Terraform configuration to use modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,80 +1,23 @@
-######   vpc configuration  ######
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source       = "./modules/vpc"
+  project_name = var.project_name
 }
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+module "iam" {
+  source       = "./modules/iam"
+  project_name = var.project_name
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+module "ecr" {
+  source       = "./modules/ecr"
+  project_name = var.project_name
 }
 
-#######  iam role for EKS cluster  ######
-
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-      Effect = "Allow"
-    }]
-  })
+module "eks" {
+  source              = "./modules/eks"
+  cluster_name        = var.cluster_name
+  subnet_ids          = module.vpc.private_subnets
+  vpc_id              = module.vpc.vpc_id
+  eks_cluster_role    = module.iam.eks_cluster_role_arn
+  eks_node_group_role = module.iam.eks_node_group_role_arn
 }
-
-#######  attach policies to the role  ######
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-#######   ecr repository ######
-resource "aws_ecr_repository" "app_repo" {
-  name = "cicd-app-repo"
-}
-
-#######   eks cluster  ######
-resource "aws_eks_cluster" "eks" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.public.id]
-  }
-}
-
-
-#######  node group for EKS cluster  ######
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "node-group"
-  node_role_arn   = aws_iam_role.eks_cluster_role.arn
-  subnet_ids      = [aws_subnet.public.id]
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
-}
-
-######## jenkins ec2 instance  ######
-resource "aws_instance" "jenkins" {
-  ami           = "ami-xxxxxxxx"
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public.id
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "Jenkins-Server"
-  }
-}
-
