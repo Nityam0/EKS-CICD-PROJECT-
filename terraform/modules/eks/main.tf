586b69d7f3459ef19d42f98eea0b84f5312c7a09
Nityam Raj <147521555+Nityam0@users.noreply.github.com>
2026-03-03 16:31:53 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -15,8 +15,8 @@ resource "aws_eks_node_group" "this" {
 
   scaling_config {
     desired_size = 2
-    max_size     = 3
     min_size     = 1
+    max_size     = 3
   }
 
   instance_types = ["t3.medium"]
