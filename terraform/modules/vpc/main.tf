8dffbbe63dcf6eaeca519e0ab00263a813a4be61
Nityam Raj <147521555+Nityam0@users.noreply.github.com>
2026-03-03 16:25:23 +0530

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,5 +1,7 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "this" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -9,18 +11,24 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "public" {
-  count                   = 2
+  count                   = var.public_subnet_count
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 8, count.index)
-  map_public_ip_on_launch = true
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
   availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-${count.index}"
+  }
 }
 
 resource "aws_subnet" "private" {
-  count             = 2
+  count             = var.private_subnet_count
   vpc_id            = aws_vpc.this.id
-  cidr_block        = cidrsubnet(aws_vpc.this.cidr_block, 8, count.index + 10)
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 10)
   availability_zone = data.aws_availability_zones.available.names[count.index]
-}
 
-data "aws_availability_zones" "available" {}
+  tags = {
+    Name = "${var.project_name}-private-${count.index}"
+  }
+}
