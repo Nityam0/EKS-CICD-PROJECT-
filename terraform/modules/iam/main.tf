a17b0e4c7c0dc4540c3e023c0c613135b4210a8b
Nityam Raj <147521555+Nityam0@users.noreply.github.com>
2026-03-03 16:28:06 +0530

    Update main.tf

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,17 +1,15 @@
+# EKS Cluster Role
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.project_name}-eks-cluster-role"
 
-  assume_role_policy = data.aws_iam_policy_document.eks_assume.json
-}
-
-data "aws_iam_policy_document" "eks_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_policy" {
@@ -19,18 +17,21 @@ resource "aws_iam_role_policy_attachment" "cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "node_group_role" {
+# Node Group Role
+resource "aws_iam_role" "eks_node_role" {
   name = "${var.project_name}-node-role"
 
-  assume_role_policy = data.aws_iam_policy_document.node_assume.json
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
-data "aws_iam_policy_document" "node_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
