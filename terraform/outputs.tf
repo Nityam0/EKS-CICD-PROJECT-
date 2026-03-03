50ff90bfe0ede03f0f0153ce8fc862c74edbdd6e
Nityam Raj <147521555+Nityam0@users.noreply.github.com>
2026-03-03 15:43:42 +0530

    Rename outputs and update values in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "jenkins_public_ip" {
-  value = aws_instance.jenkins.public_ip
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
 
-output "ecr_repo_url" {
-  value = aws_ecr_repository.app_repo.repository_url
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
 }
