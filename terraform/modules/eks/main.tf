c34034adcfd923e98a5bfb70dda3efc8730feb06
Nityam Raj <147521555+Nityam0@users.noreply.github.com>
2026-03-03 16:51:08 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,23 +1,32 @@
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
-  role_arn = var.eks_cluster_role
+  role_arn = var.cluster_role_arn
+  version  = "1.29"
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids              = var.private_subnets
+    endpoint_public_access  = true
+    endpoint_private_access = false
   }
+
+  tags = local.common_tags
 }
 
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "managed-nodes"
-  node_role_arn   = var.eks_node_group_role
-  subnet_ids      = var.subnet_ids
+  node_group_name = "primary-nodes"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.private_subnets
 
   scaling_config {
     desired_size = 2
+    max_size     = 4
     min_size     = 1
-    max_size     = 3
   }
 
   instance_types = ["t3.medium"]
+
+  tags = {
+    "k8s.io/cluster-autoscaler/enabled" = "true"
+  }
 }
